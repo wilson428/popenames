57bbf5db8b92e06a748dd67ab5ed574828f64525
Chris Wilson <christopher.e.wilson@gmail.com>
2013-03-13 18:38:55 -0400

    Adding a decimal value for roman numerals

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -4,6 +4,24 @@ import json
 from lxml.html import etree, fromstring
 from utils import download, write
 
+def roman2arabic(value):
+    if not value:
+        return None
+        
+    total = 0
+    values = { 'I': 1, 'V': 5, 'X': 10 }
+    
+    prevValue = 0
+    for char in value:
+        if values[char] > prevValue:
+            total -= prevValue
+        else:
+            total += prevValue
+        prevValue = values[char]
+    total += prevValue
+    
+    return total
+    
 roster = fromstring(download("http://www.newadvent.org/cathen/12272b.htm")).xpath("//ol/li")
 popes = []
 
@@ -25,6 +43,7 @@ for pope in roster:
     data["nicknames"] = re.findall(" \((.*?)\)", data["fullname"])
     moniker = re.findall("^(Blessed|[A-z]+\. )*(.*?)([IXV]*)$", re.sub(" \((.*?)\)", "", data["fullname"]))[0]
     data["title"],data["name"],data["number"] = [x.strip() for x in moniker]
+    data["decimal"] = roman2arabic(data["number"])
 
     #456-75 -> 456-475
     if len(data["end"]) == 2:            
